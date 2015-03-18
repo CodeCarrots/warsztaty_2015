5a59ce4782ac5a010af4734c08073361b40e6fac
soutys <soutys>
2015-03-18 19:23:54 +0100

    zadanie ze stop-wordsami

---
+++ sesje/2015-03-18/sets_06.py
@@ -0,0 +1,31 @@
+from stop_words import ALL_STOP_WORDS
+
+
+page_content = '''
+This page is devoted to various tips and tricks that help improve the performance of your Python programs. Wherever the information comes from someone else, I've tried to identify the source.
+
+Python has changed in some significant ways since I first wrote my "fast python" page in about 1996, which means that some of the orderings will have changed. I migrated it to the Python wiki in hopes others will help maintain it.
+
+You should always test these tips with your application and the specific version of the Python implementation you intend to use and not just blindly accept that one method is faster than another. See the profiling section for more details.
+
+Also new since this was originally written are packages like Cython, Pyrex, Psyco, Weave, Shed Skin and PyInline, which can dramatically improve your application's performance by making it easier to push performance-critical code into C or machine language.
+'''
+
+
+def is_stopword(word, stopwords_set):
+    '''Checks whenever a `word` is a so-called `stop-word`.
+    '''
+    pass
+
+
+separated_phrases = split_into_phrases(page_content)
+
+
+clean_words = []
+for phrase in separated_phrases:
+    pass
+print(clean_words)
+
+
+unordered_clean_words = diff_sets(separated_phrases, ALL_STOP_WORDS)
+print(unordered_clean_words)
